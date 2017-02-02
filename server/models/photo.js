bf406f59edbef77b54fe57be2f4f0d79469c30e1
wstbee <ryoubee@gmail.com>
2017-02-02 20:39:07 +0900

    ryou

--- server/models/photo.js
+++ server/models/photo.js
@@ -3,7 +3,7 @@ var Photo = db.model('photo', {
     image_path: {type: String, required: true},
     username: {type: String, required: true, default: "admin"},
     explanation: {type: String, required: false},
-	score: {type: Number, required: true, default: 0},
+    score: {type: Number, required: true, default: 0},
     gender: {type: Number, required: true, default: 0},
     date: {type: Date, required: true, default: Date.now}
 })
