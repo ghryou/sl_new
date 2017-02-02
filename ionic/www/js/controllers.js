6a083b6843776161f42399033fa36936b9166f47
JaeyunKang <kjy13411@gmail.com>
2017-02-02 15:15:14 +0900

    add basic swapping application

--- ionic/www/js/controllers.js
+++ ionic/www/js/controllers.js
@@ -56,11 +56,46 @@ angular.module('starter.controllers', [])
 		};
 
 
-
-		$http.get('js/photos.json').success(function(response){
+		$http.get('js/photos.json').success(function (response){
+			
 			$scope.photos= response.a;
-			console.log(($scope.photos.length))
+
+			var html_slide = ""
+
+			$.each($scope.photos, function (index, value) {
+
+				value.comment = "나의 데일리룩"; 
+
+				html_slide += '<li class="pane' + (index + 1) + '" id="' + value.id + '"><div class="img" pid="' + value.id + '" style="background: url(\''+ value.image +'\') no-repeat scroll center center;background-size: cover;"></div>';
+			
+				html_slide += "<div style='height:22px;'></div>";
+			
+				html_slide += '<div  style="padding-top:0px;"><!--i onclick="goScrap(' + value.id + ');	 $(this).addClass(\'md-red\');" class="material-icons md-light md-inactive star-btn">&#xE838;</i--><p style="font-size:12px;">' + (value.comment ? value.comment : "나의 데일리룩") + '</p></div><div class="like"></div><div class="dislike"></div></li>';
+			});
+
+			$("#lis").html(html_slide);
+			componentHandler.upgradeDom(); // CSS 적용
+
+			goTinder();
+
+
 		});
+
+		function goTinder() {
+
+			$("#tinderslide").jTinder({
+					onDislike: function (item) {
+					},
+					onLike: function (item) {
+					},
+					animationRevertSpeed: 200,
+					animationSpeed: 400,
+					threshold: 1,
+					likeSelector: '.like',
+					dislikeSelector: '.dislike'
+			});
+			
+		}
 
 
 		$scope.hideOptions = function(){
