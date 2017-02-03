19c02592b1e517ce84386905348c265b462761d8
wstbee <ryoubee@gmail.com>
2017-02-03 16:18:37 +0900

    ghryou photo_box server

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,8 @@ var User = db.model('user', {
     username: {type: String, required: true},
     password: {type: String, required: true, select: false},
     gender: {type: Number, required: true, default: 0},
-    insta: {type: String}
+    insta: {type: String},
+    photo_like: {type: Array, default: []},
+    photo_upload: {type: Array, default: []}
 })
 module.exports = User
